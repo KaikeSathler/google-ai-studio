fb89d52db34f4020a0c4a2276f4e8a0f5c1cf564
Kaike da Silva Sathler <104788755+KaikeSathler@users.noreply.github.com>
2024-08-02 10:27:51 -0300

    update - 02/08

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,13 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+let json = undefined;
+
+await fetch("../config.json").then(async (data) => {
+  json = await data.json();
+});
 
 var app = document.getElementById('ia');
 
-var typewriter = new Typewriter(app, {
-  delay: 10
-});
-
-typewriter.typeString('Olá, tudo bem, como posso te ajudar hoje?')
-    .start();
-
-const API_KEY = "AIzaSyDsE6K5NDNRTDGxX2xlyppT0fesCbjuxZs";
+const API_KEY = json.API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const chat = model.startChat();
@@ -17,11 +15,9 @@ const chat = model.startChat();
 function highlightCode(text) {
   const lines = text.split("```");
   let html = "";
-  console.log(lines)
   lines.forEach((linha) => {
-    console.log(linha)
-    if(hljs.getLanguage(linha.split("\n")[0])) {
-      const highlightedCode = hljs.highlight(linha, {language: linha.split("\n")[0]}).value;
+    if (hljs.getLanguage(linha.split("\n")[0])) {
+      const highlightedCode = hljs.highlight(linha, { language: linha.split("\n")[0] }).value;
       html += `<pre><code class="hljs">${highlightedCode}</code></pre>`;
     } else {
       html += markdown.toHTML(linha);
@@ -32,29 +28,30 @@ function highlightCode(text) {
 
 document.getElementById('enviar').addEventListener('click', async () => {
   const prompt = document.getElementById('prompt').value;
+  if (prompt.trim().length == 0 || prompt == undefined) {
+    return;
+  }
   document.getElementById('prompt').value = '';
 
+
   app.innerHTML += `
-    <span class="text-user" style="background-color: red;">${prompt}</span><br><br>
+    <div class="text-user"><span>${prompt}</span></div>
   `;
 
   try {
     const result = await chat.sendMessageStream(prompt);
-    const response = await result.response;
-    let text = response.text();
+    let resposta = "";
+    let span = document.createElement("div");
+    app.insertAdjacentElement("beforeend", span);
 
-    text = highlightCode(text);
-    
     for await (const chunk of result.stream) {
-      const chunkText = chunk.text();
-      text += chunkText;
-      app.innerHTML += text;
+      console.log(chunk.text())
+      const chunkText = await chunk.text();
+      resposta += chunk.text()
+      span.innerHTML = highlightCode(resposta);
     }
-
 
   } catch (error) {
     console.error("Erro na solicitação:", error);
-    typewriter.deleteAll().start();
-    typewriter.typeString("Desculpe, ocorreu um erro. Tente novamente.").start();
   }
 });
