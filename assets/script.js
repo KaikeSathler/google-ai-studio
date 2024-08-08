6d65b108569b427a3c9be7975ee7e2ddcf9e24bf
Kaike da Silva Sathler <104788755+KaikeSathler@users.noreply.github.com>
2024-08-07 22:45:46 -0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,4 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-// let json = undefined;
-
-// await fetch("../config.json").then(async (data) => {
-//   json = await data.json();
-// });
 
 var app = document.getElementById('ia');
 
@@ -11,6 +6,21 @@ const API_KEY = "AIzaSyDsE6K5NDNRTDGxX2xlyppT0fesCbjuxZs";
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const chat = model.startChat();
+const fraseAtual = "Olá, como eu posso de ajudar?";
+const tokenElement = document.getElementById('token-count');
+
+const elemento = document.createElement("div");
+elemento.className = "text-assistant";
+app.insertAdjacentElement("beforeend", elemento);
+
+const typewriter = new Typewriter(elemento, {
+  cursor: "",
+  delay: 1,
+});
+
+typewriter
+  .typeString(fraseAtual)
+  .start();
 
 function highlightCode(text) {
   const lines = text.split("```");
@@ -26,19 +36,26 @@ function highlightCode(text) {
   return html;
 }
 
+async function contarTokens(prompt) {
+  const { totalTokens } = await model.countTokens(prompt);
+  console.log(`Total de tokens: ${totalTokens}`);
+  tokenElement.innerText = `Tokens: ${totalTokens}`;
+}
+
 document.getElementById('enviar').addEventListener('click', async () => {
   const prompt = document.getElementById('prompt').value;
   if (prompt.trim().length == 0 || prompt == undefined) {
     return;
   }
-  document.getElementById('prompt').value = '';
 
+  document.getElementById('prompt').value = '';
 
   app.innerHTML += `
     <div class="text-user"><span>${prompt}</span></div>
   `;
 
   try {
+    await contarTokens(prompt);
     const result = await chat.sendMessageStream(prompt);
     let resposta = "";
     let span = document.createElement("div");
@@ -46,7 +63,6 @@ document.getElementById('enviar').addEventListener('click', async () => {
 
     for await (const chunk of result.stream) {
       console.log(chunk.text())
-      const chunkText = await chunk.text();
       resposta += chunk.text()
       span.innerHTML = highlightCode(resposta);
     }
