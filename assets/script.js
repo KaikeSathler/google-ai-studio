c1840dd5e95dc8afd252fc816bc43ce277d786a4
Kaike da Silva Sathler <104788755+KaikeSathler@users.noreply.github.com>
2024-08-26 14:15:22 -0300

    Update 26/08 - Insert message MongoDB

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,20 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 
-var app = document.getElementById('ia');
+var app = document.getElementById("ia");
 let tokenCount = 0; // Adicionei essa variável para armazenar o contador de tokens
+let tempTokenCount = 0;
+const promptElement = document.getElementById("prompt");
+const tokenElement = document.getElementById("token-count");
+
+promptElement.addEventListener("keyup", async function (e) {
+  // console.log(e.target.value)
+  const { totalTokens } = await model.countTokens(e.target.value);
+  tempTokenCount = tokenCount;
+  tokenElement.innerHTML = `Tokens: ${totalTokens + tempTokenCount}/${
+    chatbotConfig.maxOutputTokens
+  }`;
+});
 
 const safetySettings = [
   {
@@ -20,10 +32,15 @@ const chatbotConfig = {
 
 const API_KEY = "AIzaSyC9Ue8l3aXORQGNKrq19f59rClI5GG61xY";
 const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", chatbotConfig, safetySettings, system_instruction:"Você é especializado em programação. Linguagens a serem consideradas, HTML, CSS, Python, JavaScript, Java, C++, C#, PHP, Ruby, Swift, Go, Kotlin e outras focadas em Web. O modelo deve ser capaz de gerar código para diferentes tipos de aplicações, desde aplicações web até sistemas de desktop e mobile. É essencial ter um dataset grande e diversificado de código para cada linguagem. Você pode utilizar repositórios de código como GitHub, GitLab e Bitbucket, além de código de projetos open-source. Gere código de alta qualidade para todas essas linguagens exige um dataset massivo e uma arquitetura complexa, capaz de lidar com as diferentes nuances sintáticas e semânticas de cada linguagem.",});
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  chatbotConfig,
+  safetySettings,
+  system_instruction:
+    "Você é especializado em programação. Linguagens a serem consideradas, HTML, CSS, Python, JavaScript, Java, C++, C#, PHP, Ruby, Swift, Go, Kotlin e outras focadas em Web. O modelo deve ser capaz de gerar código para diferentes tipos de aplicações, desde aplicações web até sistemas de desktop e mobile. É essencial ter um dataset grande e diversificado de código para cada linguagem. Você pode utilizar repositórios de código como GitHub, GitLab e Bitbucket, além de código de projetos open-source. Gere código de alta qualidade para todas essas linguagens exige um dataset massivo e uma arquitetura complexa, capaz de lidar com as diferentes nuances sintáticas e semânticas de cada linguagem.",
+});
 const chat = model.startChat();
 const fraseAtual = "Olá, como eu posso de ajudar?";
-const tokenElement = document.getElementById('token-count');
 
 const elemento = document.createElement("div");
 elemento.className = "text-assistant";
@@ -34,21 +51,21 @@ const typewriter = new Typewriter(elemento, {
   delay: 20,
 });
 
-typewriter
-  .typeString(fraseAtual)
-  .start();
+typewriter.typeString(fraseAtual).start();
 
 function highlightCode(text) {
   const lines = text.split("```");
   let html = "";
   lines.forEach((linha) => {
     if (hljs.getLanguage(linha.split("\n")[0])) {
-      const highlightedCode = hljs.highlight(linha, { language: linha.split("\n")[0] }).value;
+      const highlightedCode = hljs.highlight(linha, {
+        language: linha.split("\n")[0],
+      }).value;
       html += `<pre><code class="hljs">${highlightedCode}</code></pre>`;
     } else {
       html += markdown.toHTML(linha);
     }
-  })
+  });
   return html;
 }
 
@@ -59,43 +76,58 @@ async function contarTokens(prompt) {
   tokenElement.innerHTML = `Tokens: ${tokenCount}/${chatbotConfig.maxOutputTokens}`;
 }
 
-document.getElementById('enviar').addEventListener('click', async () => {
-  const prompt = document.getElementById('prompt').value;
+document.getElementById("enviar").addEventListener("click", async () => {
+  let prompt = promptElement.value;
   if (prompt.trim().length == 0 || prompt == undefined) {
     return;
   }
 
+  await contarTokens(prompt);
+
   if (tokenCount >= chatbotConfig.maxOutputTokens) {
-    alert("Você alcançou o limite de tokens. Por favor, aguarde um momento antes de enviar outra mensagem.");
+    alert(
+      "Você alcançou o limite de tokens. Por favor, aguarde um momento antes de enviar outra mensagem."
+    );
     return;
   }
 
-  document.getElementById('prompt').value = '';
+  document.getElementById("prompt").value = "";
 
   app.innerHTML += `
     <div class="text-user"><div class="text-user-content"><span>${prompt}</span></div></div>
-  `;
+    `;
 
+  let resposta = "";
   try {
-    await contarTokens(prompt);
     const result = await chat.sendMessageStream(prompt);
-    let resposta = "";
     let span = document.createElement("div");
     app.insertAdjacentElement("beforeend", span);
 
     for await (const chunk of result.stream) {
-      console.log(chunk.text())
-      resposta += chunk.text()
+      console.log(chunk.text());
+      resposta += chunk.text();
       span.innerHTML = highlightCode(resposta);
+      app.scrollTo(0, app.scrollHeight);
+      console.log(app.scrollHeight);
     }
-
+    console.log(resposta)
+    fetch('http://localhost:8003/api/message', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+  
+      body: JSON.stringify({
+        mensagem: resposta
+      }),
+    })
   } catch (error) {
     console.error("Erro na solicitação:", error);
-  }
+  } 
+
 });
 
-// Resetar o contador de tokens quando o usuário tenta enviar uma mensagem após alcançar o limite
-document.getElementById('prompt').addEventListener('input', () => {
+document.getElementById("prompt").addEventListener("input", () => {
   if (tokenCount >= chatbotConfig.maxOutputTokens) {
     tokenCount = 0;
   }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,46 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://kaikesathler7:g0wvze58TC9CyLWm@cluster0.8eiqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const uri = "mongodb+srv://kaike:GsrQPjbSXQbhyPNu@cluster0.pz7sm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(uri);
+
+const tabelaMensagens = new mongoose.Schema({
+  mensagem: {
+    type: "String",
+    required: true
+  }
+});
+
+const colecaoMensagem = mongoose.model("mensagens", tabelaMensagens);
+
+app.post("/api/message", (req, res) => {
+  console.log(req.body)
+  const mensagem = req.body.mensagem;
+  if(!mensagem) {
+    return res.sendStatus(500);
+  }
+  new colecaoMensagem({
+    mensagem: mensagem
+  }).save();
+  res.sendStatus(200);
+})
+
+app.listen(8003, (req, res) => {
+  console.log("Server ligado na porta 8003!");
+})
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -15,12 +55,6 @@ async function run() {
   await client.db("admin").command({ ping: 1 });
   const db = client.db("gemini_db").collection("usuario")
   console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  try {
-    const buscarUsuarios = await db.find({}).toArray()
-    console.log(buscarUsuarios)
-  } catch (e) {
-    console.error(e)
-  }
 }
 run().catch(console.dir);
 
