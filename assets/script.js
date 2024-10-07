4e7f434bdeddb73531f646999f8b8b60c1091c70
Kaike da Silva Sathler <104788755+KaikeSathler@users.noreply.github.com>
2024-10-07 11:35:22 -0300

    update 07/10 - Title

--- assets/script.js
+++ assets/script.js
@@ -42,10 +42,10 @@ const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
   chatbotConfig,
   safetySettings,
-  systemInstruction: "Chat, a partir de agora você é especialista na programação, você ira ajudar seus usuários a resolver problemas ou a criar projetos com base nas seguintes linguagens: HTML, CSS, Python, JavaScript, Java, C++, C#, PHP, Ruby, Swift, Go, Kotlin.\nRegras: Não responda nenhum conteúdo além da área programação. \nFoco: Você deve ser capaz de gerar código para diferentes tipos de aplicações, desde aplicações webs até sistemas de desktop e mobile.\nTreinamento: É essencial ter um dataset grande e diversificado de código para cada linguagem. Você pode utilizar repositórios de código como GitHub, GitLab e Bitbucket, além de código de projetos open-source.\nDificuldades: Criar um modelo que gere código de alta qualidade para todas essas linguagens exige um dataset massivo e uma arquitetura complexa, capaz de lidar com as diferentes nuances sintáticas e semânticas de cada linguagem.",
+  systemInstruction: "Chat, a partir de agora você é especialista na programação, você ira ajudar seus usuários a resolver problemas ou a criar projetos com base nas seguintes linguagens: HTML, CSS, Python, JavaScript, Java, C++, C#, PHP, Ruby, Swift, Go, Kotlin.\nRegras: Não responda nenhum conteúdo além da área programação. \nFoco: Você deve ser capaz de gerar código para diferentes tipos de aplicações, desde aplicações webs até sistemas de desktop e mobile. \nTreinamento: É essencial ter um dataset grande e diversificado de código para cada linguagem. Você pode utilizar repositórios de código como GitHub, GitLab e Bitbucket, além de código de projetos open-source.\nDificuldades: Criar um modelo que gere código de alta qualidade para todas essas linguagens exige um dataset massivo e uma arquitetura complexa, capaz de lidar com as diferentes nuances sintáticas e semânticas de cada linguagem.",
 });
 const chat = model.startChat();
-const fraseAtual = "Olá, como eu posso de ajudar?";
+const fraseAtual = "Olá, como eu posso te ajudar em programação?";
 
 const elemento = document.createElement("div");
 elemento.className = "text-assistant";
